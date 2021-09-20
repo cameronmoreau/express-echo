0b833a37f929c9f7967ad75ecc9d570057567d8d
Cameron Moreau <cameron.cm6@gmail.com>
2021-09-20 13:25:39 -0500

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# express-echo
+
+This is a simple echo server using express that connects to ngrok.
+
+## Usage
+
+```sh
+# Setup
+yarn
+
+# (optional) Setup ngrok
+export NGROK_HOSTNAME=xxx
+export NGROK_AUTHTOKEN=xxx
+
+yarn start
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const ngrok = process.env.NGROK_AUTHTOKEN ? require("ngrok") : null;
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello 👋"));
+
+app.post("/post", (req, res) => {
+  const output = {
+    body: req.body,
+    headers: req.headers,
+  };
+
+  console.log(output);
+  res.send(output);
+});
+
+const server = app.listen(port, () => {
+  console.log(`🚀  Server listening on port ${server.address().port}`);
+});
+
+if (ngrok) {
+  ngrok
+    .connect({
+      addr: port,
+      hostname: process.env.NGROK_HOSTNAME,
+      subdomain: process.env.NGROK_SUBDOMAIN,
+      authtoken: process.env.NGROK_AUTHTOKEN,
+    })
+    .then((url) => {
+      console.log(`💳  App URL to see the demo in your browser: ${url}/`);
+    })
+    .catch((err) => {
+      if (err.code === "ECONNREFUSED") {
+        console.log(`⚠️  Connection refused at ${err.address}:${err.port}`);
+      } else {
+        console.log(`⚠️ Ngrok error: ${JSON.stringify(err)}`);
+      }
+      process.exit(1);
+    });
+}
