94cb3c1fded01f83e62a2c18e33ac3475017f336
Cameron Moreau <cameron.cm6@gmail.com>
2021-09-20 13:41:28 -0500

    add urlencoded middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("Hello 👋"));
 
